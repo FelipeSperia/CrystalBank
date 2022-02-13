8721a1e25e0b103bb748c923482f3f9d9fe9d55f
FelipeSperia <felipe.speria@hotmail.com>
2022-02-12 21:00:25 -0300

    1 commit banco crystal

---
+++ crystaL_bank_app/src/App.js
@@ -0,0 +1,24 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src="logo.png" className="App-logo" alt="logo" />
+        <p> Crystal Bank </p>
+      </header>
+      <body className="App-body">
+        <div>
+          <p>Login</p>
+          <input></input>
+        </div>
+        <div>
+          <p>Senha</p>
+          <input></input>
+        </div>
+      </body>
+    </div>
+  );
+}
+
+export default App;
